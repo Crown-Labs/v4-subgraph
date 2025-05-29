360f9d9076755a22ef7f283545945badf1c85423
0xBishop <bishop@crown-labs.io>
2025-05-29 15:44:08 +0700

    feat: update liquidate position

--- src/mappings/bank.ts
+++ src/mappings/bank.ts
@@ -1,14 +1,24 @@
 import { BigInt, log } from '@graphprotocol/graph-ts'
 
 import {
+  EnableCollateral as EnableCollateralEvent,
   LiquidatePosition as LiquidatePositionEvent,
   SetConfigBorrowToken as SetConfigBorrowTokenEvent,
 } from '../types/KittycornBank/KittycornBank'
-import { BorrowAsset, LiquidatePosition, LiquidationPositionDaily, Token } from '../types/schema'
+import {
+  BankPosition,
+  BorrowAsset,
+  LiquidatePosition,
+  LiquidationPositionDaily,
+  MappingTokenIdPool,
+  Pool,
+  Token,
+} from '../types/schema'
 import { exponentToBigDecimal } from '../utils'
 import { getSubgraphConfig, SubgraphConfig } from '../utils/chains'
 import { ADDRESS_ZERO, ONE_BI, ZERO_BD, ZERO_BI } from '../utils/constants'
 import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol, fetchTokenTotalSupply } from '../utils/token'
+// import { fetchGetPoolAndPositionInfo } from '../utils/position'
 // The subgraph handler must have this signature to be able to handle events,
 // however, we invoke a helper in order to inject dependencies for unit tests.
 export function handleConfigBorrowToken(event: SetConfigBorrowTokenEvent): void {
@@ -17,6 +27,10 @@ export function handleConfigBorrowToken(event: SetConfigBorrowTokenEvent): void 
 
 export function handleLiquidatePosition(event: LiquidatePositionEvent): void {
   handleLiquidatePositionHelper(event)
+}
+
+export function handleEnableCollateral(event: EnableCollateralEvent): void {
+  handleEnableCollateralHelper(event)
 }
 
 export function handleConfigBorrowTokenHelper(
@@ -84,24 +98,70 @@ export function handleLiquidatePositionHelper(
   const tokenId = event.params.tokenId.toString()
   const positionId = event.params.positionId.toString()
 
-  const id = tokenId + '-' + positionId
-  let position = LiquidatePosition.load(id)
+  const mappingTokenIdPool = MappingTokenIdPool.load(tokenId)
 
-  if (position === null) {
-    position = new LiquidatePosition(id)
-    position.positionId = ZERO_BI
-    position.tokenId = ZERO_BI
-    position.owner = ADDRESS_ZERO
-    position.liquidator = ADDRESS_ZERO
-    position.repayToken = ADDRESS_ZERO
-    position.liquidatePrice = ZERO_BI
-    position.positionValue = ZERO_BI
-    position.repayValue = ZERO_BI
-    position.liquidateFeeValue = ZERO_BI
-    position.protocolFee = ZERO_BI
-    position.txHash = ADDRESS_ZERO
-    position.timestamp = ZERO_BI
+  if (mappingTokenIdPool === null) {
+    log.error('handleLiquidatePositionHelper: mappingTokenIdPool not found for tokenId {}', [tokenId])
+    return
   }
+
+  const pool = Pool.load(mappingTokenIdPool.pool)
+  if (pool === null) {
+    log.error('handleLiquidatePositionHelper: pool not found for poolId {}', [mappingTokenIdPool.pool])
+    return
+  }
+
+  let repayToken = Token.load(event.params.repayToken.toHexString())
+  if (repayToken === null) {
+    repayToken = new Token(event.params.repayToken.toHexString())
+    repayToken.symbol = fetchTokenSymbol(
+      event.params.repayToken,
+      subgraphConfig.tokenOverrides,
+      subgraphConfig.nativeTokenDetails,
+    )
+    repayToken.name = fetchTokenName(
+      event.params.repayToken,
+      subgraphConfig.tokenOverrides,
+      subgraphConfig.nativeTokenDetails,
+    )
+    repayToken.totalSupply = fetchTokenTotalSupply(event.params.repayToken)
+    const decimals = fetchTokenDecimals(
+      event.params.repayToken,
+      subgraphConfig.tokenOverrides,
+      subgraphConfig.nativeTokenDetails,
+    )
+
+    // bail if we couldn't figure out the decimals
+    if (decimals === null) {
+      log.debug('mybug the decimal on repay token was null', [])
+      return
+    }
+
+    repayToken.decimals = decimals
+    repayToken.derivedETH = ZERO_BD
+    repayToken.volume = ZERO_BD
+    repayToken.volumeUSD = ZERO_BD
+    repayToken.feesUSD = ZERO_BD
+    repayToken.untrackedVolumeUSD = ZERO_BD
+    repayToken.totalValueLocked = ZERO_BD
+    repayToken.totalValueLockedUSD = ZERO_BD
+    repayToken.totalValueLockedUSDUntracked = ZERO_BD
+    repayToken.txCount = ZERO_BI
+    repayToken.poolCount = ZERO_BI
+    repayToken.whitelistPools = []
+  }
+
+  // load token0 and token1
+  const token0 = Token.load(pool.token0)
+  const token1 = Token.load(pool.token1)
+
+  if (token0 === null || token1 === null || repayToken === null) {
+    log.error('handleLiquidatePositionHelper: token0 or token1 not found for poolId {}', [mappingTokenIdPool.pool])
+    return
+  }
+
+  const id = tokenId + '-' + positionId
+
   const decimalsToken = fetchTokenDecimals(
     event.params.repayToken,
     subgraphConfig.tokenOverrides,
@@ -119,20 +179,28 @@ export function handleLiquidatePositionHelper(
     .times(event.params.liquidatePrice.toBigDecimal())
     .div(exponentToBigDecimal(decimalsToken))
 
-  position.positionId = event.params.positionId
-  position.tokenId = event.params.tokenId
-  position.owner = event.params.owner.toHexString()
-  position.liquidator = event.transaction.from.toHexString()
-  position.repayToken = event.params.repayToken.toHexString()
-  position.liquidatePrice = event.params.liquidatePrice
-  position.positionValue = event.params.positionValue
-  position.repayValue = BigInt.fromString(repayValue.toString())
-  position.liquidateFeeValue = event.params.liquidateFeeValue
-  position.protocolFee = event.params.protocolFee
-  position.txHash = event.transaction.hash.toHexString()
-  position.timestamp = event.block.timestamp
-
-  position.save()
+  const liqPosition = new LiquidatePosition(id)
+  liqPosition.tokenId = event.params.tokenId
+  liqPosition.positionId = event.params.positionId
+  liqPosition.owner = event.params.owner.toHexString()
+  liqPosition.liquidator = event.transaction.from.toHexString()
+  liqPosition.repayToken = repayToken.id
+  liqPosition.liquidatePrice = event.params.liquidatePrice
+  liqPosition.positionValue = event.params.positionValue
+  liqPosition.repayValue = BigInt.fromString(repayValue.toString())
+  liqPosition.liquidateFeeValue = event.params.liquidateFeeValue
+  liqPosition.protocolFee = event.params.protocolFee
+  liqPosition.txHash = event.transaction.hash.toHexString()
+  liqPosition.timestamp = event.block.timestamp
+  liqPosition.token0 = token0.id
+  liqPosition.token1 = token1.id
+  liqPosition.feeTier = pool.feeTier
+  liqPosition.tickSpacing = pool.tickSpacing
+  liqPosition.hooks = pool.hooks
+  liqPosition.tickLower = mappingTokenIdPool.tickLower
+  liqPosition.tickUpper = mappingTokenIdPool.tickUpper
+  liqPosition.sqrtPrice = pool.sqrtPrice
+  liqPosition.tick = ZERO_BI
 
   const timestamp = event.block.timestamp.toI32()
 
@@ -148,9 +216,148 @@ export function handleLiquidatePositionHelper(
     positionDaily.protocolFeeAccumulate = ZERO_BI
   }
   positionDaily.totalCount = positionDaily.totalCount.plus(ONE_BI)
-  positionDaily.liquidateValueAccumulate = positionDaily.liquidateValueAccumulate.plus(position.positionValue)
-  positionDaily.liquidateFeeValueAccumulate = positionDaily.liquidateFeeValueAccumulate.plus(position.liquidateFeeValue)
-  positionDaily.protocolFeeAccumulate = positionDaily.protocolFeeAccumulate.plus(position.protocolFee)
+  positionDaily.liquidateValueAccumulate = positionDaily.liquidateValueAccumulate.plus(liqPosition.positionValue)
+  positionDaily.liquidateFeeValueAccumulate = positionDaily.liquidateFeeValueAccumulate.plus(
+    liqPosition.liquidateFeeValue,
+  )
+  positionDaily.protocolFeeAccumulate = positionDaily.protocolFeeAccumulate.plus(liqPosition.protocolFee)
 
+  liqPosition.save()
   positionDaily.save()
+  repayToken.save()
+}
+
+export function handleEnableCollateralHelper(event: EnableCollateralEvent): void {
+  return
+  // const tokenOverrides = subgraphConfig.tokenOverrides
+  // const nativeTokenDetails = subgraphConfig.nativeTokenDetails
+  // log.info('handleEnableCollateralHelper', [])
+  // log.info('event.params.tokenId: {}, event.params.positionId: {}', [
+  //   event.params.tokenId.toString(),
+  //   event.params.positionId.toString(),
+  // ])
+
+  const tokenId = event.params.tokenId.toString()
+  const positionId = event.params.positionId.toString()
+
+  const id = tokenId + '-' + positionId
+  let bankPosition = BankliqPosition.load(id)
+  if (bankPosition === null) {
+    bankPosition = new BankPosition(id)
+    bankliqPosition.tokenId = ZERO_BI
+    bankliqPosition.positionId = ZERO_BI
+    bankliqPosition.currency0 = ADDRESS_ZERO
+    bankliqPosition.currency1 = ADDRESS_ZERO
+    bankliqPosition.fee = ZERO_BI
+    bankliqPosition.tickSpacing = ZERO_BI
+    bankliqPosition.hooks = ADDRESS_ZERO
+    bankliqPosition.positionInfo = ZERO_BI
+  }
+
+  // fetch poolKey by tokenId
+  // const poolKey = fetchGetPoolAndPositionInfo(
+  //   Address.fromString(subgraphConfig.kittycornPositionManagerAddress),
+  //   event.params.tokenId,
+  // )
+  // if (poolKey === null) {
+  //   log.debug('mybug the poolKey was null', [])
+  //   return
+  // }
+  // load token0 and token1
+  // let token0 = Token.load(poolKey.currency0)
+  // let token1 = Token.load(poolKey.currency1)
+
+  // fetch info if null
+  // if (token0 === null) {
+  //   // log.info('token0=============================================', [])
+  //   token0 = new Token(poolKey.currency0)
+  //   token0.symbol = fetchTokenSymbol(Address.fromString(poolKey.currency0), tokenOverrides, nativeTokenDetails)
+  //   token0.name = fetchTokenName(Address.fromString(poolKey.currency0), tokenOverrides, nativeTokenDetails)
+  //   token0.totalSupply = fetchTokenTotalSupply(Address.fromString(poolKey.currency0))
+  //   const decimals = fetchTokenDecimals(Address.fromString(poolKey.currency0), tokenOverrides, nativeTokenDetails)
+
+  //   // bail if we couldn't figure out the decimals
+  //   if (decimals === null) {
+  //     log.debug('mybug the decimal on token 0 was null', [])
+  //     return
+  //   }
+
+  //   token0.decimals = decimals
+  //   token0.derivedETH = ZERO_BD
+  //   token0.volume = ZERO_BD
+  //   token0.volumeUSD = ZERO_BD
+  //   token0.feesUSD = ZERO_BD
+  //   token0.untrackedVolumeUSD = ZERO_BD
+  //   token0.totalValueLocked = ZERO_BD
+  //   token0.totalValueLockedUSD = ZERO_BD
+  //   token0.totalValueLockedUSDUntracked = ZERO_BD
+  //   token0.txCount = ZERO_BI
+  //   token0.poolCount = ZERO_BI
+  //   token0.whitelistPools = []
+  // }
+  // if (token1 === null) {
+  //   // log.info('token1=============================================', [])
+  //   token1 = new Token(poolKey.currency1)
+  //   token1.symbol = fetchTokenSymbol(Address.fromString(poolKey.currency1), tokenOverrides, nativeTokenDetails)
+  //   token1.name = fetchTokenName(Address.fromString(poolKey.currency1), tokenOverrides, nativeTokenDetails)
+  //   token1.totalSupply = fetchTokenTotalSupply(Address.fromString(poolKey.currency1))
+  //   const decimals = fetchTokenDecimals(Address.fromString(poolKey.currency1), tokenOverrides, nativeTokenDetails)
+
+  //   if (decimals === null) {
+  //     log.debug('mybug the decimal on token 1 was null', [])
+  //     return
+  //   }
+
+  //   token1.decimals = decimals
+  //   token1.derivedETH = ZERO_BD
+  //   token1.volume = ZERO_BD
+  //   token1.volumeUSD = ZERO_BD
+  //   token1.untrackedVolumeUSD = ZERO_BD
+  //   token1.feesUSD = ZERO_BD
+  //   token1.totalValueLocked = ZERO_BD
+  //   token1.totalValueLockedUSD = ZERO_BD
+  //   token1.totalValueLockedUSDUntracked = ZERO_BD
+  //   token1.txCount = ZERO_BI
+  //   token1.poolCount = ZERO_BI
+  //   token1.whitelistPools = []
+  // }
+  // log.info('handleEnableCollateralHelper: token0.id: {}', [Address.fromString(token0.id).toHexString()])
+  // log.info('handleEnableCollateralHelper: token1.id: {}', [Address.fromString(token1.id).toHexString()])
+  // log.info('handleEnableCollateralHelper: poolKey.fee: {}', [poolKey.fee.toString()])
+  // log.info('handleEnableCollateralHelper: poolKey.tickSpacing: {}', [poolKey.tickSpacing.toString()])
+  // log.info('handleEnableCollateralHelper: poolKey.hooks: {}', [Address.fromString(poolKey.hooks).toHexString()])
+
+  // bankliqPosition.tokenId = event.params.tokenId
+  // bankliqPosition.positionId = event.params.positionId
+  // bankliqPosition.currency0 = Address.fromString(token0.id).toHexString()
+  // bankliqPosition.currency1 = Address.fromString(token1.id).toHexString()
+  // bankliqPosition.fee = poolKey.fee
+  // bankliqPosition.tickSpacing = poolKey.tickSpacing
+  // bankliqPosition.hooks = poolKey.hooks.toHexString()
+  // bankliqPosition.positionInfo = poolKey.positionInfo
+
+  // log.info('handleEnableCollateralHelper: bankPosition: {}', [bankliqPosition.id])
+  // log.info('handleEnableCollateralHelper: bankliqPosition.tokenId: {}, bankliqPosition.positionId: {}', [
+  //   bankliqPosition.tokenId.toString(),
+  //   bankliqPosition.positionId.toString(),
+  // ])
+  // log.info('handleEnableCollateralHelper: bankliqPosition.currency0: {}, bankliqPosition.currency1: {}', [
+  //   bankliqPosition.currency0,
+  //   bankliqPosition.currency1,
+  // ])
+  // log.info('handleEnableCollateralHelper: bankliqPosition.fee: {}, bankliqPosition.tickSpacing: {}', [
+  //   bankliqPosition.fee.toString(),
+  //   bankliqPosition.tickSpacing.toString(),
+  // ])
+
+  // bankliqPosition.tokenId = BigInt.fromString('1')
+  // bankliqPosition.positionId = BigInt.fromString('1')
+  // bankliqPosition.currency0 = ADDRESS_ZERO
+  // bankliqPosition.currency1 = ADDRESS_ZERO
+  // bankliqPosition.fee = ZERO_BI
+  // bankliqPosition.tickSpacing = ZERO_BI
+  // bankliqPosition.hooks = ADDRESS_ZERO
+  // token0.save()
+  // token1.save()
+  bankliqPosition.save()
 }
--- src/utils/position.ts
+++ src/utils/position.ts
@@ -1,153 +1,58 @@
-import { Address, BigInt } from '@graphprotocol/graph-ts'
+// import { Address, BigInt, Bytes, log } from '@graphprotocol/graph-ts'
 
-import { ERC20 } from '../types/PoolManager/ERC20'
-import { ERC20NameBytes } from '../types/PoolManager/ERC20NameBytes'
-import { ERC20SymbolBytes } from '../types/PoolManager/ERC20SymbolBytes'
-import { ADDRESS_ZERO, ZERO_BI } from './constants'
-import { isNullEthValue } from './index'
-import { NativeTokenDetails } from './nativeTokenDetails'
-import { getStaticDefinition, StaticTokenDefinition } from './staticTokenDefinition'
+// import { PositionManager } from '../types/KittycornBank/PositionManager'
+// import { ERC20 } from '../types/PoolManager/ERC20'
+// import { ERC20NameBytes } from '../types/PoolManager/ERC20NameBytes'
+// import { ERC20SymbolBytes } from '../types/PoolManager/ERC20SymbolBytes'
+// import { ADDRESS_ZERO, ZERO_BI } from './constants'
+// import { isNullEthValue } from './index'
+// import { NativeTokenDetails } from './nativeTokenDetails'
+// import { getStaticDefinition, StaticTokenDefinition } from './staticTokenDefinition'
 
-export function fetchTokenSymbol(
-  tokenAddress: Address,
-  tokenOverrides: StaticTokenDefinition[],
-  nativeTokenDetails: NativeTokenDetails,
-): string {
-  if (tokenAddress.equals(Address.fromString(ADDRESS_ZERO))) {
-    return nativeTokenDetails.symbol
-  }
-  // try with the static definition
-  const staticTokenDefinition = getStaticDefinition(tokenAddress, tokenOverrides)
-  if (staticTokenDefinition != null) {
-    return staticTokenDefinition.symbol
-  }
+// export class PoolKeyAndPositionInfo {
+//   currency0: string
+//   currency1: string
+//   fee: BigInt
+//   tickSpacing: BigInt
+//   hooks: Address
+//   positionInfo: BigInt
+// }
 
-  const contract = ERC20.bind(tokenAddress)
-  const contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress)
+// export function fetchGetPoolAndPositionInfo(
+//   positionManagerAddress: Address,
+//   tokenId: BigInt,
+// ): PoolKeyAndPositionInfo | null {
+//   const contract = PositionManager.bind(positionManagerAddress)
+//   const getPoolAndPositionInfoResult = contract.try_getPoolAndPositionInfo(tokenId)
 
-  // try types string and bytes32 for symbol
-  let symbolValue = 'unknown'
-  const symbolResult = contract.try_symbol()
-  if (symbolResult.reverted) {
-    const symbolResultBytes = contractSymbolBytes.try_symbol()
-    if (!symbolResultBytes.reverted && !isNullEthValue(symbolResultBytes.value.toHexString())) {
-      // for broken pairs that have no symbol function exposed
-      if (!isNullEthValue(symbolResultBytes.value.toHexString())) {
-        symbolValue = symbolResultBytes.value.toString()
-      }
-    }
-  } else {
-    symbolValue = symbolResult.value
-  }
+// log.info('2 success2 tokenid {}========================================', [tokenId.toString()])
 
-  return symbolValue
-}
-
-export function fetchTokenName(
-  tokenAddress: Address,
-  tokenOverrides: StaticTokenDefinition[],
-  nativeTokenDetails: NativeTokenDetails,
-): string {
-  if (tokenAddress.equals(Address.fromString(ADDRESS_ZERO))) {
-    return nativeTokenDetails.name
-  }
-  // try with the static definition
-  const staticTokenDefinition = getStaticDefinition(tokenAddress, tokenOverrides)
-  if (staticTokenDefinition != null) {
-    return staticTokenDefinition.name
-  }
-
-  const contract = ERC20.bind(tokenAddress)
-  const contractNameBytes = ERC20NameBytes.bind(tokenAddress)
-
-  // try types string and bytes32 for name
-  let nameValue = 'unknown'
-  const nameResult = contract.try_name()
-  if (nameResult.reverted) {
-    const nameResultBytes = contractNameBytes.try_name()
-    if (!nameResultBytes.reverted) {
-      // for broken exchanges that have no name function exposed
-      if (!isNullEthValue(nameResultBytes.value.toHexString())) {
-        nameValue = nameResultBytes.value.toString()
-      }
-    }
-  } else {
-    nameValue = nameResult.value
-  }
-
-  return nameValue
-}
-
-export function fetchTokenTotalSupply(tokenAddress: Address): BigInt {
-  if (tokenAddress.equals(Address.fromString(ADDRESS_ZERO))) {
-    return ZERO_BI
-  }
-  const contract = ERC20.bind(tokenAddress)
-  let totalSupplyValue = BigInt.zero()
-  const totalSupplyResult = contract.try_totalSupply()
-  if (!totalSupplyResult.reverted) {
-    totalSupplyValue = totalSupplyResult.value
-  }
-  return totalSupplyValue
-}
-
-export function fetchTokenDecimals(
-  tokenAddress: Address,
-  tokenOverrides: StaticTokenDefinition[],
-  nativeTokenDetails: NativeTokenDetails,
-): BigInt | null {
-  if (tokenAddress.equals(Address.fromString(ADDRESS_ZERO))) {
-    return nativeTokenDetails.decimals
-  }
-  // try with the static definition
-  const staticTokenDefinition = getStaticDefinition(tokenAddress, tokenOverrides)
-  if (staticTokenDefinition) {
-    return staticTokenDefinition.decimals
-  }
-
-  const contract = ERC20.bind(tokenAddress)
-  // try types uint8 for decimals
-  const decimalResult = contract.try_decimals()
-
-  if (!decimalResult.reverted) {
-    if (decimalResult.value.lt(BigInt.fromI32(255))) {
-      return decimalResult.value
-    }
-  }
-
-  return null
-}
-
-// export function
-// (positionManagerAddress: Address, tokenId: BigInt): string {
-//   // if (tokenAddress.equals(Address.fromString(ADDRESS_ZERO))) {
-//   //   return nativeTokenDetails.name
-//   // }
-//   // // try with the static definition
-//   // const staticTokenDefinition = getStaticDefinition(tokenAddress, tokenOverrides)
-//   // if (staticTokenDefinition != null) {
-//   //   return staticTokenDefinition.name
-//   // }
-
-//   const contract = positionManager.bind(positionManagerAddress)
-
-//   const contract = ERC20.bind(tokenAddress)
-//   const contractNameBytes = ERC20NameBytes.bind(tokenAddress)
-
-//   // try types string and bytes32 for name
-//   let nameValue = 'unknown'
-//   const nameResult = contract.try_name()
-//   if (nameResult.reverted) {
-//     const nameResultBytes = contractNameBytes.try_name()
-//     if (!nameResultBytes.reverted) {
-//       // for broken exchanges that have no name function exposed
-//       if (!isNullEthValue(nameResultBytes.value.toHexString())) {
-//         nameValue = nameResultBytes.value.toString()
-//       }
+// log.info('getPoolAndPositionInfoResult.value.value0.currency0.toHexString(): {}', [
+//   getPoolAndPositionInfoResult.value.value0.currency0.toHexString(),
+// ])
+// log.info('getPoolAndPositionInfoResult.value.value0.currency1.toHexString(): {}', [
+//   getPoolAndPositionInfoResult.value.value0.currency1.toHexString(),
+// ])
+// log.info('getPoolAndPositionInfoResult.value.value0.fee: {}', [
+//   getPoolAndPositionInfoResult.value.value0.fee.toString(),
+// ])
+// log.info('getPoolAndPositionInfoResult.value.value0.tickSpacing: {}', [
+//   getPoolAndPositionInfoResult.value.value0.tickSpacing.toString(),
+// ])
+// log.info('getPoolAndPositionInfoResult.value.value1.toHexString(): {}', [
+//   getPoolAndPositionInfoResult.value.value1.toHexString(),
+// ])
+//   if (!getPoolAndPositionInfoResult.reverted) {
+//     return {
+//       currency0: getPoolAndPositionInfoResult.value.value0.currency0.toHexString(),
+//       currency1: getPoolAndPositionInfoResult.value.value0.currency1.toHexString(),
+//       fee: BigInt.fromI32(getPoolAndPositionInfoResult.value.value0.fee),
+//       tickSpacing: BigInt.fromI32(getPoolAndPositionInfoResult.value.value0.tickSpacing),
+//       hooks: getPoolAndPositionInfoResult.value.value0.hooks,
+//       positionInfo: getPoolAndPositionInfoResult.value.value1,
 //     }
 //   } else {
-//     nameValue = nameResult.value
+//     log.error('fetchGetPoolAndPositionInfo: getPoolAndPositionInfo reverted for tokenId {}', [tokenId.toString()])
+//     return null
 //   }
-
-//   return nameValue
 // }
