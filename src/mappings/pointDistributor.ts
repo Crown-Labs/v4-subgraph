3e952702b795d70a5764818c385f46d6e5a70482
0xBishop <bishop@crown-labs.io>
2026-02-17 14:17:15 +0700

    feat: separate point reward for test

--- src/mappings/pointDistributor.ts
+++ src/mappings/pointDistributor.ts
@@ -11,7 +11,7 @@ export function handleClaimed(event: Claimed): void {
   const claimId = eventId(event.transaction.hash, event.logIndex)
   const claim = new PointClaim(claimId)
   claim.to = event.params.to
-  claim.amount = event.params.amount
+  claim.amount = event.params.amount.abs()
   claim.nonce = event.params.nonce
   claim.timestamp = event.block.timestamp
   claim.transaction = transaction.id
